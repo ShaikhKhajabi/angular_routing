dbe7359460fb5b1faa1d5819795e56fc41c98a87
Khajabi Shaikh <khajabi0410@gmail.com>
2022-06-02 12:39:28 +0530

    imported router module in product.module.ts

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 
 import { SharedModule } from '../shared/shared.module';
-import { RouterModule } from '@angular/router';
+
 
 @NgModule({
   imports: [
