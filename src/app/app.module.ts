0ce29e57555dd4f0ddba05a9db1704674123bb99
Khajabi Shaikh <khajabi0410@gmail.com>
2022-06-02 13:07:24 +0530

    define routing module in new app-routing module for clean code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
+
 
 // Imports for loading & configuring the in-memory web api
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { ProductData } from './products/product-data';
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { WelcomeComponent } from './home/welcome.component';
-import { PageNotFoundComponent } from './page-not-found.component';
+
 
 /* Feature Modules */
 import { ProductModule } from './products/product.module';
@@ -17,21 +17,16 @@ import { UserModule } from './user/user.module';
 import { MessageModule } from './messages/message.module';
 
 @NgModule({
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     HttpClientModule,
     InMemoryWebApiModule.forRoot(ProductData, { delay: 1000 }),
-    RouterModule.forRoot([
-      { path: 'welcome', component: WelcomeComponent },
-      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-      { path: '**', component: PageNotFoundComponent },
-    ]),
-
     ProductModule,
     UserModule,
     MessageModule,
+    AppRoutingModule
   ],
-  declarations: [AppComponent, WelcomeComponent, PageNotFoundComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
