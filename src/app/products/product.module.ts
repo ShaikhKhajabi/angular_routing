f412ae2efbcb9eff9a64319f5f93c591f8acafac
Khajabi Shaikh <khajabi0410@gmail.com>
2022-06-03 15:59:05 +0530

    grouping routes n made componentless routes

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -15,20 +15,29 @@ import { SharedModule } from '../shared/shared.module';
   imports: [
     SharedModule,
     RouterModule.forChild([
-      { path: 'products', component: ProductListComponent },
       {
-        path: 'products/:id',
-        component: ProductDetailComponent,
-        resolve: { resolvedData: ProductResolver },
-      },
-      {
-        path: 'products/:id/edit',
-        component: ProductEditComponent,
-        resolve: { resolvedData: ProductResolver },
+        path: 'products',
         children: [
-          { path: '', redirectTo: 'info', pathMatch: 'full' },
-          { path: 'info', component: ProductEditInfoComponent },
-          { path: 'tags', component: ProductEditTagsComponent },
+          {
+            path: '',
+            component: ProductListComponent
+          },
+          {
+            path: ':id',
+            component: ProductDetailComponent,
+            resolve: { resolvedData: ProductResolver },
+          },
+          {
+            path: ':id/edit',
+            component: ProductEditComponent,
+            resolve: { resolvedData: ProductResolver },
+            children: [
+              { path: '', redirectTo: 'info', pathMatch: 'full' },
+              { path: 'info', component: ProductEditInfoComponent },
+              { path: 'tags', component: ProductEditTagsComponent },
+            ]
+          }
+
         ],
       },
     ]),
@@ -41,4 +50,4 @@ import { SharedModule } from '../shared/shared.module';
     ProductEditTagsComponent
   ],
 })
-export class ProductModule {}
+export class ProductModule { }
