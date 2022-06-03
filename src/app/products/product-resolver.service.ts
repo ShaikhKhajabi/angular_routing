bdadbadd4a37485c60489b0780e7b662f4a804ba
Khajabi Shaikh <khajabi0410@gmail.com>
2022-06-03 14:30:27 +0530

    obtained data for child routes

--- src/app/products/product-resolver.service.ts
+++ src/app/products/product-resolver.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-import { from, Observable, of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 import { ProductResolved } from './product';
