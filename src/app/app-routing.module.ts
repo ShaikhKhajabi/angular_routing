72d98b4292ff20d56af4a4bc2a3d998b10cfd864
Khajabi Shaikh <khajabi0410@gmail.com>
2022-06-06 16:27:50 +0530

    preloaded the feature modules by using preloads all module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,26 +1,26 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { PreloadAllModules, RouterModule } from '@angular/router';
 
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
 import { AuthGuard } from './user/auth.guard';
 
-const ROUTES = [
-  { path: 'welcome', component: WelcomeComponent },
-  {
-    path: 'products',
-    canLoad: [AuthGuard],
-    loadChildren: () =>
-      import('./products/product.module').then(m => m.ProductModule)
-  },
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: '**', component: PageNotFoundComponent },
-]
 
 @NgModule({
   declarations: [],
   imports: [
-    RouterModule.forRoot(ROUTES),
+    RouterModule.forRoot([
+      { path: 'welcome', component: WelcomeComponent },
+      {
+        path: 'products',
+        canActivate: [AuthGuard],
+        loadChildren: () =>
+          import('./products/product.module').then(m => m.ProductModule)
+      },
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: '**', component: PageNotFoundComponent },
+    ], {preloadingStrategy: PreloadAllModules}),
+    
   ],
   exports: [RouterModule]
 })
